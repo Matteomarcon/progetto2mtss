f470ed722295976f60590663fa03c7d2cd92436d
Matteomarcon <fermi.marcon.matteo@gmail.com>
2025-04-28 13:02:07 +0200

    Setup iniziale close #1

---
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////////// 
+// Matteo Marcon 2101060
+// Roberto Piovesan 2101065
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class IntegerToRomanTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
