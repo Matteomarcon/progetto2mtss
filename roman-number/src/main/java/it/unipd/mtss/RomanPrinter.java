f838808c3bc9e9ce6db7904899a2905e04a7a6ad
Matteomarcon <fermi.marcon.matteo@gmail.com>
2025-05-07 15:35:34 +0200

    RomanPrinter close #3

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -6,11 +6,47 @@
 package it.unipd.mtss;
 
 public class RomanPrinter {
-	public static String print(int num){
-		return printAsciiArt(IntegerToRoman.convert(num));
-	}
-	private static String printAsciiArt(String romanNumber){
-		
-		return null;
-	}
+public static String print(int num){
+return printAsciiArt(IntegerToRoman.convert(num));
+}
+private static String printAsciiArt(String romanNumber){
+String asciiArt = "";
+
+String[][] lettere = {
+{
+" |\    /| ", // M
+" |‾‾\  ",  // D
+" /‾‾\ ",  // C
+" |     ",  // L
+" \  / ",  // X
+" \    / ",  // V
+" ‾|‾ " // I
+},
+{
+" | \  / | ", // M
+" |   | ",  // D
+" |    ",  // C
+" |     ",  // L
+"  \/  ",  // X
+"  \  /  ",  // V
+"  |  " // I
+},
+{
+" |  \/  | ", // M
+" |__/  ",  // D
+" \__/ ",  // C
+" |___ ",  // L
+" /  \ ",  // X
+"   \/   ",  // V
+" _|_ " // I
+},
+};
+for( int i=0; i<3; i++) {
+for (char c : romanNumber.toCharArray()) {
+asciiArt += lettere[i]["MDCLXVI".indexOf(c)];
+}
+asciiArt += "\n";
+}
+return asciiArt;
+}
 }
