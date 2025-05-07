8c46c2901ffd17f41ec5844f61fa359c3f5db367
[LuckyBerto] <[robypiovesan04@gmail.com]>
2025-05-07 14:32:14 +0200

    IntegerToRoman close #2

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,8 +6,19 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
-	public static String convert(int number){
-		// TODO
-		return null;
-	}
+public static String convert(int number){
+String[] lettereRomane = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+int[] interiCorrispondenti = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+if (number < 1 || number > 1000) {
+throw new IllegalArgumentException("Devi inserire un numero tra 1 e 1000!");
+}
+String conversione = "";
+for (int i=0; i<interiCorrispondenti.length; i++) {
+while (number >= interiCorrispondenti[i]) {
+number = number - interiCorrispondenti[i];
+conversione = conversione + lettereRomane[i];
+}
+}
+return conversione;
+}
 }
