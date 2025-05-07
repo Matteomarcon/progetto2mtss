77b08e73156dfcff07dd931c90c598fdc60a8d73
[LuckyBerto] <[robypiovesan04@gmail.com]>
2025-05-07 16:52:23 +0200

    Test primi 6 numeri interi close #5

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -8,9 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 public class IntegerToRomanTest {
 @Test
-public void testPrimiTreInteri() {
+public void testPrimiSeiInteri() {
 assertEquals(IntegerToRoman.convert(1),"I");
 assertEquals(IntegerToRoman.convert(2),"II");
 assertEquals(IntegerToRoman.convert(3),"III");
+assertEquals(IntegerToRoman.convert(4),"IV");
+assertEquals(IntegerToRoman.convert(5),"V");
+assertEquals(IntegerToRoman.convert(6),"VI");
 }
 }
--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -8,9 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 public class RomanPrinterTest {
 @Test
-public void printerTestPrimiTreInteri() {
+public void printerTestPrimiSeiInteri() {
 assertEquals(RomanPrinter.print(1)," ‾|‾ \n  |  \n _|_ \n");
 assertEquals(RomanPrinter.print(2)," ‾|‾  ‾|‾ \n  |    |  \n _|_  _|_ \n");
 assertEquals(RomanPrinter.print(3)," ‾|‾  ‾|‾  ‾|‾ \n  |    |    |  \n _|_  _|_  _|_ \n");
+assertEquals(RomanPrinter.print(4)," ‾|‾  \\    / \n  |    \\  /  \n _|_    \\/   \n");
+assertEquals(RomanPrinter.print(5)," \\    / \n  \\  /  \n   \\/   \n");
+assertEquals(RomanPrinter.print(6)," \\    /  ‾|‾ \n  \\  /    |  \n   \\/    _|_ \n");
+
 }
 }
